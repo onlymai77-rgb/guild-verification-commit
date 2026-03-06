584a702f11cac108d5802c3dece8e1bd9c544724
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:11:27 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:11:10.392Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T14:11:31.577Z
+Commit: Resolve null pointer exception in user service
