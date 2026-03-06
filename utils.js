fd67b304018cc955c5a28226e4ca8724e571b289
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:12:02 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:12:00.907Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T14:12:05.978Z
+Commit: Add export functionality for reports
