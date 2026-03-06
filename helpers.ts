f7804786bce33923b7d2d7460fdc7a812404bf41
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:10:46 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:10:39.324Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T14:10:50.427Z
+Commit: Correct typo in configuration file
