e2804b0cacf8a25d781074eb2faf3760b34e3bfb
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:11:11 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:11:08.322Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T14:11:14.605Z
+Commit: Fix memory leak in event handlers
