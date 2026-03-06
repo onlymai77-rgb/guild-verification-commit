729865fbffbc0dca6e1ceff56c01ca382d990c5a
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:10:07 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:09:51.397Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T14:10:11.160Z
+Commit: Fix CSS styling bug on mobile devices
