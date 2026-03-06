747cfcb9fc78606c203f3991e3137a8e3d961f4e
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:11:08 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:10:56.625Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T14:11:12.565Z
+Commit: Resolve null pointer exception in user service
