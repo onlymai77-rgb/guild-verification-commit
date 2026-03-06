ecce830fdd4c05aa2ce150cc8de98295a465025f
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:08:04 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:07:48.452Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T14:08:07.806Z
+Commit: Correct typo in configuration file
