cab40ec139bf40884fba3e8ecccccd0141d5db9b
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:11:49 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:11:32.659Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T14:11:53.549Z
+Commit: Introduce real-time notifications
