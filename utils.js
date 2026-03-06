4ec6909ac34720a5ef3431847f6c674ce52b0407
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:10:44 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:10:09.192Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T14:10:48.475Z
+Commit: Fix CSS styling bug on mobile devices
