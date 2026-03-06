e36d7aeb0f265ad3000183227026ba5d5e0f25bd
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:09:45 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:09:40.927Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T14:09:48.727Z
+Commit: Resolve null pointer exception in user service
