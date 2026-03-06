7fe2b143972e69e386cfeafdc8e65cd03a9e595f
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:10:15 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:10:16.948Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T14:10:19.021Z
+Commit: Resolve null pointer exception in user service
