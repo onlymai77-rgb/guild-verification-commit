62c4182606cdf6726569f33bb2802b58f131f3ca
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:11:59 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:11:35.768Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T14:12:02.953Z
+Commit: Fix broken links in documentation
