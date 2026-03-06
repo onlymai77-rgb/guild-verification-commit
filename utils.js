c437de0a5718b7afc76265c4f4376df9e1ad24d4
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:07:45 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:07:28.355Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T14:07:49.304Z
+Commit: Fix race condition in async operations
