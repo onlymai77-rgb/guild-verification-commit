9a738a8b51e85b7baec9beaefcbaffe518c7950e
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:07:31 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:07:09.655Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T14:07:34.920Z
+Commit: Patch API endpoint error handling
