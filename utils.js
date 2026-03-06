be00d621e66542c1bb1101a2b26972fdbde758a3
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:09:41 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:09:38.858Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T14:09:44.908Z
+Commit: Resolve timezone issues in date handling
