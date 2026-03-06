c76603d9a95b4580b81d0ca9139c8ec6275eaaea
onlymai77-rgb <onlymai77@gmail.com>
2026-03-06 17:09:32 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:09:11.103Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T14:09:35.875Z
+Commit: Fix broken links in documentation
